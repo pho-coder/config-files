404ecf91892e84431b0445667abe7e533a753e25
Phoenix Zhang <pho.coderzhang@gmail.com>
2014-05-12 16:45:35 +0800

    add osx emacs init.el

---
+++ emacs.d/init.el
@@ -0,0 +1,49 @@
+;; fix the PATH variable
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(when window-system (set-exec-path-from-shell-PATH))
+
+(require 'package)
+
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(defvar my-packages '(starter-kit
+                      starter-kit-lisp
+                      starter-kit-bindings
+                      starter-kit-eshell
+;;                      clojure-mode
+;;                      clojure-test-mode
+;;                      nrepl
+                      color-theme
+                      clojurescript-mode
+                      emmet-mode
+                      cider))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(require 'color-theme)
+
+;(color-theme-initialize)
+;(color-theme-dark-blue2)
+(color-theme-calm-forest)
+;(color-theme-clarity)
+;(color-theme-clarity)
+
+(add-hook 'sgml-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook 'emmet-mode)
+
+(defun my-fullscreen ()
+  (interactive)
+  (x-send-client-message
+   nil 0 nil "_NET_WM_STATE" 32
+   '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
+
+(global-set-key [f11] 'my-fullscreen)
+(put 'upcase-region 'disabled nil)
